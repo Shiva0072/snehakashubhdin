ac256a4648bf7974fb715da08d25f550ed148258
Shivam <devshiva.iit.kgp@gmail.com>
2023-01-14 20:11:37 +0530

    changes_done_for_deployment

--- src/utils/getFiles.js
+++ src/utils/getFiles.js
@@ -1,24 +1,33 @@
+const temp = [
+  "https://ik.imagekit.io/dr2kyfwww/test/1.jpeg",
+  "https://ik.imagekit.io/dr2kyfwww/test/2.png",
+];
+
 function importAll(r, imgs) {
   r.keys().forEach((key) => imgs.push(key));
 }
 
+// const getFiles = () => {
+//   const images = require.context(
+//     "../Assets/images/",
+//     false,
+//     /\.(png|jpe?g|svg)$/
+//   );
+
+//   const imgs = [];
+//   importAll(images, imgs);
+
+//   const new_imgs = [];
+//   imgs.forEach((img) => {
+//     new_imgs.push(require(`../Assets/images/${img.substr(2)}`));
+//   });
+
+//   console.log("justed obtained imga;  : ", new_imgs);
+//   return new_imgs;
+// };
+
 const getFiles = () => {
-  const images = require.context(
-    "../Assets/images/",
-    false,
-    /\.(png|jpe?g|svg)$/
-  );
-
-  const imgs = [];
-  importAll(images, imgs);
-
-  const new_imgs = [];
-  imgs.forEach((img) => {
-    new_imgs.push(require(`../Assets/images/${img.substr(2)}`));
-  });
-
-  console.log("justed obtained imga;  : ", new_imgs);
-  return new_imgs;
+  return temp;
 };
 
 export default getFiles;
